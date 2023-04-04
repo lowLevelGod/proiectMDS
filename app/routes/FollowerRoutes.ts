fb3daf4409b69111354910087a7f48ba088574ec
lowLevelGod <petrepopa19@gmail.com>
2023-04-04 21:35:39 +0300

    done get all followers and all following

--- app/routes/FollowerRoutes.ts
+++ app/routes/FollowerRoutes.ts
@@ -6,3 +6,4 @@ export const followerRouter = express.Router();
 followerRouter.post('/follow', authenticationController.isAuthenticated, followerController.request);
 followerRouter.patch('/follow', authenticationController.isAuthenticated, followerController.accept);
 followerRouter.delete('/follow', authenticationController.isAuthenticated, followerController.delete);
+followerRouter.get('/follow', authenticationController.isAuthenticated, followerController.get);
