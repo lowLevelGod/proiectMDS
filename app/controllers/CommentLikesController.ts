28c3b8f219eecf89402a8c9a15e562ccae388b03
cosmin <cosminmoarcas01@gmail.com>
2023-04-09 13:59:57 +0300

    done endpoints for comment likes

--- app/controllers/CommentLikesController.ts
+++ app/controllers/CommentLikesController.ts
@@ -22,7 +22,7 @@ export class CommentLikesController {
     }
 
     getLikes(req: Request, res: Response, next: NextFunction) {
-        const commentId = req.body.commentId;   
+        const commentId = req.params.commentId;   
 
         if (!commentId || typeof commentId != 'string') {
             const error = craftError(errorCodes.notFound, "Comment not found!");
@@ -32,7 +32,7 @@ export class CommentLikesController {
         knexInstance
             .select('*')
             .from('CommentLikes')
-            .where('commentId', '=', commentId)
+            .where('commentId', commentId)
             .then(rows => {
                 if (rows.length === 0) {
                     const error = craftError(errorCodes.notFound, "No likes found for this comment!");
@@ -49,5 +49,63 @@ export class CommentLikesController {
                 const error = craftError(errorCodes.other, "Please try again!");
                 return res.status(500).json({ error, content: undefined });
             })
+    }
+
+    deleteLike(req: Request, res: Response, next: NextFunction) {
+        const commentLike: CommentLike = {
+            userId: req.session.user!.id,
+            commentId: req.params.commentId
+        };
+
+        knexInstance
+            .from('CommentLikes')
+            .where(commentLike)
+            .del()
+            .then(rowsDeleted => {
+                if (rowsDeleted === 0) {
+                    const error = craftError(errorCodes.notFound, "No like found for this comment and user!");
+                    return res.status(404).json({ error, content: undefined });
+                }
+                return res.status(200).json({ error: undefined, content: undefined });
+            })
+            .catch(err => {
+                console.error(err.message);
+                const error = craftError(errorCodes.other, "Please try again!");
+                return res.status(500).json({ error, content: undefined });
+            })
+    }
+
+    getCommentLikesCount(req: Request, res: Response, next: NextFunction) {
+        const commentId = req.params.commentId;
+
+        // Check if comment exists
+        knexInstance('Comments')
+            .select('id')
+            .where('id', commentId)
+            .then(rows => {
+                if (rows.length === 0) {
+                    const error = craftError(errorCodes.notFound, "Comment not found!");
+                    return res.status(404).json({ error, content: undefined });
+                }
+            })
+            .then(() => {
+                knexInstance
+                    .count()
+                    .from('CommentLikes')
+                    .where('commentId', commentId)
+                    .then(count => {
+                        return res.status(200).json({ error: undefined, content: count });
+                    })
+                    .catch(err => {
+                        console.error(err.message);
+                        const error = craftError(errorCodes.other, "Please try again!");
+                        return res.status(500).json({ error, content: undefined });
+                    });
+            })
+            .catch(err =>{
+                console.error(err.message);
+                const error = craftError(errorCodes.other, "Please try again!");
+                return res.status(500).json({ error, content: undefined });
+            });
     }
 }
