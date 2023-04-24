dcf77c2d244043158794fe14dcac2dbeb50832ce
lowLevelGod <petrepopa19@gmail.com>
2023-04-24 20:05:33 +0300

    removed required login for get post

--- app/routes/PostRoutes.ts
+++ app/routes/PostRoutes.ts
@@ -5,7 +5,7 @@ export const postRouter = express.Router();
 
 postRouter.post('/posts', authenticationController.isAuthenticated, uploadMediaPosts, postController.create);
 postRouter.delete('/posts/:id', authenticationController.isAuthenticated, postController.delete);
-postRouter.get('/posts/:id', authenticationController.isAuthenticated, postController.getSinglePost);
+postRouter.get('/posts/:id', postController.getSinglePost);
 postRouter.get('/posts', postController.getPostsByUser);
 postRouter.get('/posts/:id/media', postController.getPostMedia);
 postRouter.patch('/posts/:id', authenticationController.isAuthenticated, postController.patch);
