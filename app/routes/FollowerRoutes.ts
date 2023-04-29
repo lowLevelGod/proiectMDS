bf1db5cbdb5992b23473331975d09dd7cde76b66
lowLevelGod <petrepopa19@gmail.com>
2023-04-29 20:14:45 +0300

    removed required authentication for get followers

--- app/routes/FollowerRoutes.ts
+++ app/routes/FollowerRoutes.ts
@@ -6,4 +6,4 @@ export const followerRouter = express.Router();
 followerRouter.post('/follow', authenticationController.isAuthenticated, followerController.request);
 followerRouter.patch('/follow', authenticationController.isAuthenticated, followerController.accept);
 followerRouter.delete('/follow/:userId', authenticationController.isAuthenticated, followerController.delete);
-followerRouter.get('/follow', authenticationController.isAuthenticated, followerController.get);
+followerRouter.get('/follow', followerController.get);
