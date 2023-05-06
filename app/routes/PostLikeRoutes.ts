6907d7e42d12b4d9dde1a0ea86066a357fff98e2
lowLevelGod <54854545+lowLevelGod@users.noreply.github.com>
2023-05-06 21:07:17 +0300

    fixed wrong routes

--- app/routes/PostLikeRoutes.ts
+++ app/routes/PostLikeRoutes.ts
@@ -3,7 +3,7 @@ import { authenticationController, postLikeController } from "../utils/globals";
 
 export const commentLikeRouter = express.Router();
 
-commentLikeRouter.post('/comments/:id/likes', authenticationController.isAuthenticated, postLikeController.create);
-commentLikeRouter.get('/comments/:id/likes', postLikeController.getPostLikes); 
-commentLikeRouter.get('/comments/:id/likes/count', postLikeController.getPostLikesCount);
-commentLikeRouter.delete('/comments/:id/likes', authenticationController.isAuthenticated, postLikeController.deleteLike);
+commentLikeRouter.post('/posts/:id/likes', authenticationController.isAuthenticated, postLikeController.create);
+commentLikeRouter.get('/posts/:id/likes', postLikeController.getPostLikes); 
+commentLikeRouter.get('/posts/:id/likes/count', postLikeController.getPostLikesCount);
+commentLikeRouter.delete('/posts/:id/likes', authenticationController.isAuthenticated, postLikeController.deleteLike);
